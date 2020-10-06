532da6ca450f3c95c26298aca9f69e4e774f87c1
gjimgo <gjimenez@unab.edu.co>
2020-10-05 19:24:27 -0500

    This is solution from Course Project 2, about Exploratory Data Analysis on Coursera's Data Science Specialization.

---
+++ BaltimoreCity.R
@@ -0,0 +1,8 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+head(NEI)
+head(SCC)
+# Load the packages used in the exploratory analysis
+library(ggplot2)
+library(plyr)
+
